504ea92060e04a0b72e8822e9d2b5556d4598ea0
TheGabloxia <70188611+TG609@users.noreply.github.com>
2021-10-21 01:12:48 -0500

    Add files via upload

---
+++ Funciones/FPS_Boost.lua
@@ -0,0 +1,2 @@
+local cap = setfpscap or set_fps_cap or fps_cap_set
+cap(999)
---
+++ Funciones/Inyectado.lua
@@ -0,0 +1,9 @@
+game.StarterGui:SetCore("SendNotification", {
+        Title = 'Inyeccion';
+                            Text = 'TGX inyecto correctamente!';
+                            Icon = 'rbxassetid://7202946737';
+                            Duration = 5;
+                            
+                            Button1 = 'Ok';
+                          
+                        })
